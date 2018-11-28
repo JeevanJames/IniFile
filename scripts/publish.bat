f21e240d48055b637d9b9507d6d4bf9fa059cb30
Jeevan James <jeevanjj@live.com>
2018-11-29 03:11:33 +0530

    Added scripts folder

---
+++ scripts/publish.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if ("%1"=="") goto error
+
+set VERSION=%1
+echo %VERSION%
+dotnet nuget push .\src\IniFile\bin\Release\IniFile.NET.%VERSION%.nupkg -s https://api.nuget.org/v3/index.json
+goto done
+
+:error
+echo Specify the version number
+
+:done
