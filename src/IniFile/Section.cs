01e9ea127263f531a3203b56c832dc34df069779
Jeevan James <jeevanjj@live.com>
2018-11-30 03:36:19 +0530

    Updated license headers.

--- src/IniFile/Section.cs
+++ src/IniFile/Section.cs
@@ -17,6 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #endregion
+
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
